b62429a983a01c0d580e3a7da09c7bf02e225b53
Mycelium <SporeNFTs@proton.me>
2024-11-03 16:52:27 +1000

    follow infura example

--- utils/infura.ts
+++ utils/infura.ts
@@ -1,24 +1,25 @@
 import { ethers } from 'ethers';
 import * as jose from 'jose';
+import * as fs from 'fs';
 
 export async function createInfuraProvider() {
   try {
-    // Clean and format the private key by replacing \n with actual newlines
-    const cleanPrivateKey = process.env.JWT_PRIVATE_KEY
-      .replace(/\\n/g, '\n')
-      .replace(/"/g, ''); // Remove any quotes if present
-
     // Create JWT token with required fields
-    const token = await new jose.SignJWT({ 
+    const token = await new jose.SignJWT({
+      aud: 'infura.io',  // Required audience claim
       exp: Math.floor(Date.now() / 1000) + 3600, // 1 hour from now
-      aud: 'infura.io'
     })
       .setProtectedHeader({ 
         alg: 'RS256',
         typ: 'JWT',
         kid: process.env.JWT_KEY_NAME
       })
-      .sign(await jose.importPKCS8(cleanPrivateKey, 'RS256'));
+      .sign(await jose.importPKCS8(
+        process.env.JWT_PRIVATE_KEY
+          .replace(/\\n/g, '\n')  // Convert string newlines to actual newlines
+          .replace(/"/g, ''),     // Remove quotes
+        'RS256'
+      ));
 
     // Create provider with JWT auth header
     const fetchRequest = new ethers.FetchRequest(process.env.INFURA_URL);
